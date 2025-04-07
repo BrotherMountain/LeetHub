f2f6ccca3756977150f786833e55ca958cb4505c
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-04-07 20:24:47 +0900

    Time: 2 ms (84.92%), Space: 46.3 MB (11.02%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0, j = 0;
+        for (; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        for (; j < nums.length; j++) {
+            nums[j] = 0;
+        }
+    }
+}
