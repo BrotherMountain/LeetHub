46c9d2880eee98df9c097221a7562581e52a86f3
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-04-08 19:35:39 +0900

    Time: 0 ms (100%), Space: 41.5 MB (43.68%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
@@ -4,12 +4,10 @@ class Solution {
         int[] prefix = new int[gain.length + 1];
         prefix[0] = 0;
 
-        for (int i = 1; i < gain.length; i++) {
+        for (int i = 1; i <= gain.length; i++) {
             prefix[i] = prefix[i - 1]+ gain[i-1];
             ans = Math.max(ans, prefix[i]);
         }
-        prefix[gain.length] = prefix[gain.length - 1] + gain[gain.length - 1];
-        ans = Math.max(ans, prefix[gain.length]);
         return ans;
     }
 }
