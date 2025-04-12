b1629eab54b797cdbe8fbdfb3cde32fd7271a808
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-04-12 20:01:42 +0900

    Time: 98 ms (23.32%), Space: 113.8 MB (5.17%) - LeetHub

--- 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        HashSet<Integer> winnerCounts = new HashSet<>();
+        HashSet<Integer> winnerSet = new HashSet<>();
         HashMap<Integer, Integer> loserCounts = new HashMap<>();
         List<List<Integer>> ans = new ArrayList<>();
         for (int[] match : matches) {
-            winnerCounts.add(match[0]);
+            winnerSet.add(match[0]);
             loserCounts.put(match[1], loserCounts.getOrDefault(match[1], 0) + 1);
         }
         List<Integer> loser = new ArrayList<>();
@@ -12,13 +12,13 @@ class Solution {
             if (loserCounts.containsKey(match[1])) {
                 if (loserCounts.get(match[1]) == 1) {
                     loser.add(match[1]);
-                    winnerCounts.remove(match[1]);
+                    winnerSet.remove(match[1]);
                 } else if (loserCounts.get(match[1]) >= 2) {
-                    winnerCounts.remove(match[1]);
+                    winnerSet.remove(match[1]);
                 }
             }
         }
-        List<Integer> winner = new ArrayList<>(winnerCounts.stream().toList());
+        List<Integer> winner = new ArrayList<>(winnerSet);
         Collections.sort(winner);
         Collections.sort(loser);
         ans.add(winner);
