f90d87f94bfce680f3edf769c42dfea609c15c5a
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-06-10 17:16:54 +0900

    Time: 379 ms (5.05%), Space: 187.9 MB (21.58%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -3,19 +3,26 @@ class Solution {
     Set<Integer> seen = new HashSet<>();
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         for (int[] edge : edges) {
-            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
-            graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
+            if (!graph.containsKey(edge[0])) {
+                graph.put(edge[0], new ArrayList<>());
+            }
+            if (!graph.containsKey(edge[1])) {
+                graph.put(edge[1], new ArrayList<>());
+            }
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
         }
         return dfs(source, destination);
     }
 
     public boolean dfs(int curr, int dest) {
         if (curr == dest) return true;
-        if (seen.contains(curr)) return false;
-        seen.add(curr);
-        for (Integer neighbor : graph.get(curr)) {
-            if (dfs(neighbor, dest)) {
-                return true;
+        if (!seen.contains(curr)) {
+            seen.add(curr);
+            for (Integer neighbor : graph.get(curr)) {
+                if (dfs(neighbor, dest)) {
+                    return true;
+                }
             }
         }
         return false;
