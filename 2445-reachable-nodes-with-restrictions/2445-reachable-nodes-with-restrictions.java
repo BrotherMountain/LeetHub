22eea807bfa8a864a77e50b8719f153a6561fe25
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-06-10 18:37:28 +0900

    Time: 84 ms (67.57%), Space: 121.4 MB (34.12%) - LeetHub

--- 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.java
+++ 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.java
@@ -8,14 +8,8 @@ class Solution {
         }
 
         for (int[] edge : edges) {
-            if (!graph.containsKey(edge[0])) {
-                graph.put(edge[0], new ArrayList<>());
-            }
-            if (!graph.containsKey(edge[1])) {
-                graph.put(edge[1], new ArrayList<>());
-            }
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
+            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+            graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
         }
         return dfs(0);
     }
