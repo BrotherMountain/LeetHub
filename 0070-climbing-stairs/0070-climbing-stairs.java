631e0557efe3c9067b2b3e2d076401d62549b5ec
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-07-01 10:47:30 +0900

    Time: 0 ms (100%), Space: 40.5 MB (38.84%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    Map<Integer, Integer> memo = new HashMap<>();
+
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        int result = climbStairs(n - 1) + climbStairs(n - 2);
+        memo.put(n, result);
+
+        return result;
+    }
+}
