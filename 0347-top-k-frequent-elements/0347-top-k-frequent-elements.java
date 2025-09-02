2cb1819b459dfa5f3e8afbbbc32238dd4bfcf8e8
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-09-02 11:52:27 +0900

    Time: 15 ms (38.44%), Space: 49.2 MB (13.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,21 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> count = new HashMap<>();
-        for (int num : nums) {
-            count.put(num, count.getOrDefault(num, 0) + 1);
+        Map<Integer,Integer> counts = new HashMap<>();
+        for(int num: nums){
+            counts.put(num, counts.getOrDefault(num,0) + 1);
         }
-        PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> count.get(n1) - count.get(n2));
-        for (int i : count.keySet()) {
-            heap.offer(i);
-            if (heap.size() > k) {
+
+        PriorityQueue<Integer>heap = new PriorityQueue<>((n1,n2)-> counts.get(n1)-counts.get(n2));
+        for(int num: counts.keySet()){
+            heap.add(num);
+            if(heap.size()>k){
                 heap.remove();
             }
         }
 
         int[] ans = new int[k];
-        for (int i = 0; i < k; i++) {
+
+        for(int i =0; i < k; i++){
             ans[i] = heap.remove();
         }
+
         return ans;
     }
 }
