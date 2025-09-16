a0065b02b722b4ce0f4ba9e0e951192136ef5020
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-09-16 14:39:19 +0900

    Time: 2 ms (83.85%), Space: 45.7 MB (21.09%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -1,28 +1,27 @@
-
 class Solution {
-    public List<Integer> largestValues(TreeNode root) {
-        if (root == null) {
-            return new ArrayList<>();
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        List<Integer> ans = new ArrayList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            int currMax = Integer.MIN_VALUE;
-            int currentLength = queue.size();
-            for (int i = 0; i < currentLength; i++) {
-                TreeNode node = queue.remove();
-                currMax = Math.max(currMax, node.val);
-                if (node.left != null) {
-                    queue.add(node.left);
-                }
-                if (node.right != null) {
-                    queue.add(node.right);
-                }
-            }
-
-            ans.add(currMax);
-        }
-        return ans;
+  public List<Integer> largestValues(TreeNode root) {
+    if (root == null) {
+      return new ArrayList<>();
     }
+    Queue<TreeNode> queue = new LinkedList<>();
+    List<Integer> ans = new ArrayList<>();
+    queue.add(root);
+
+    while (!queue.isEmpty()) {
+      int currentLength = queue.size();
+      int val = Integer.MIN_VALUE;
+      for (int i = 0; i < currentLength; i++) {
+        TreeNode node = queue.remove();
+        val = Math.max(val, node.val);
+        if (node.left != null) {
+          queue.add(node.left);
+        }
+        if (node.right != null) {
+          queue.add(node.right);
+        }
+      }
+      ans.add(val);
+    }
+    return ans;
+  }
 }
