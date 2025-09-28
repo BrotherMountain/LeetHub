50f4b00815443bf3966126bcfe619a44ab7828ab
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-09-28 11:23:24 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,8 +1,8 @@
 SELECT
-    DISTINCT v.article_id as id
+    DISTINCT v.author_id as id
 FROM
     Views v
 WHERE
     v.author_id = v.viewer_id
 ORDER BY
-    v.article_id ASC
+    v.article_id DESC
