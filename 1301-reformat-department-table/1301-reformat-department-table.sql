4c502aac1aaa726d1776ccbde958eb68fe47d4d2
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-10-03 12:31:22 +0900

    Time: 478 ms (82.28%), Space: 0B (100%) - LeetHub

---
+++ 1301-reformat-department-table/1301-reformat-department-table.sql
@@ -0,0 +1,22 @@
+# 문제 해석: 각 달에 ID 열과 수익 열이 있도록 리포맷팅하기
+# 문제 접근
+# CASE로 접근, 없으면 NULL 있으면 revenue, 총합 집어넣기
+SELECT
+    id,
+    SUM(CASE WHEN month = 'Jan' THEN revenue END) AS Jan_Revenue,
+    SUM(CASE WHEN month = 'Feb' THEN revenue END) AS Feb_Revenue,
+    SUM(CASE WHEN month = 'Mar' THEN revenue END) AS Mar_Revenue,
+    SUM(CASE WHEN month = 'Apr' THEN revenue END) AS Apr_Revenue,
+    SUM(CASE WHEN month = 'May' THEN revenue END) AS May_Revenue,
+    SUM(CASE WHEN month = 'Jun' THEN revenue END) AS Jun_Revenue,
+    SUM(CASE WHEN month = 'Jul' THEN revenue END) AS Jul_Revenue,
+    SUM(CASE WHEN month = 'Aug' THEN revenue END) AS Aug_Revenue,
+    SUM(CASE WHEN month = 'Sep' THEN revenue END) AS Sep_Revenue,
+    SUM(CASE WHEN month = 'Oct' THEN revenue END) AS Oct_Revenue,
+    SUM(CASE WHEN month = 'Nov' THEN revenue END) AS Nov_Revenue,
+    SUM(CASE WHEN month = 'Dec' THEN revenue END) AS Dec_Revenue
+FROM
+    Department d
+GROUP BY
+    id
+
