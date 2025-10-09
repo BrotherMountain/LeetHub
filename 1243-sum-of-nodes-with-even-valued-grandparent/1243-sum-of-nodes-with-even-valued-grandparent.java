c2a82b5f85dba70075da0e799fc39eebea2524a8
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-10-09 10:50:47 +0900

    Time: 1 ms (100%), Space: 46.8 MB (11.22%) - LeetHub

--- 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
@@ -1,16 +1,21 @@
+//15분 문제 풀이 실패
+//
 class Solution {
-    int solve(TreeNode root, int parent, int gParent) {
-        if (root == null) {
-            return 0;
-        }
-        
-        // Iterate over the child with updated values of parent and grandparent.
-        return solve(root.left, root.val, parent) 
-                + solve(root.right, root.val, parent)
-                + (gParent % 2 != 0 ? 0 : root.val);
+  public int sumEvenGrandparent(TreeNode root) {
+    return solve(root, -1, -1);
+  }
+
+  private int solve(TreeNode root, int parent, int gParent) {
+    if (root == null) {
+      return 0;
     }
 
-    public int sumEvenGrandparent(TreeNode root) {
-        return solve(root, -1, -1);
-    }
+    // 후위 순회 방식
+    // root 기준 왼쪽 자식들의 조부모 짝수 값 반환
+    int left = solve(root.left, root.val, parent);
+    // root 기준 오른쪽 자식들의 조부모 짝수 값 반환
+    int right = solve(root.right, root.val, parent);
+    // root 본인의 값 반환
+    return left + right +(gParent % 2 == 0 ? root.val : 0);
+  }
 }
