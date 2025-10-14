206ac9ae5e5a978c16f4a80d3fbcf86efb0905a4
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-10-15 08:11:36 +0900

    Time: 623 ms (90.9%), Space: 0B (100%) - LeetHub

--- 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -1,22 +1,17 @@
 # 문제 접근: Products 와 Orders의 product_id를 하나의 테이블로 만들어야함 그런 다음 두 테이블의 product_id를 바탕으로 unit을 합쳐야함
 
-WITH MonthlyOrders AS (
-    SELECT
-        p.product_name,
-        o.unit,
-        SUM(o.unit) OVER (PARTITION BY p.product_id) AS total_unit_in_feb
-    FROM
-        Products p
-    JOIN
-        Orders o ON p.product_id = o.product_id
-    WHERE
-        DATE_FORMAT(o.order_date, '%Y-%m') = '2020-02'
-)
-
-SELECT DISTINCT
-    product_name,
-    total_unit_in_feb AS unit
+SELECT
+    p.product_name,
+    SUM(o.unit) AS unit
 FROM
-    MonthlyOrders
+    Products p
+JOIN
+    Orders o
+ON
+    p.product_id = o.product_id
 WHERE
-    total_unit_in_feb >= 100;
+    DATE_FORMAT(o.order_date, '%Y-%m') = '2020-02'
+GROUP BY
+    p.product_id, p.product_name
+HAVING
+    SUM(o.unit) >= 100
