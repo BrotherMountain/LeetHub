9e885d0b7bec465dffb968048bed8e3c48f7f6ff
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-10-19 23:46:34 +0900

    Time: 3 ms (14.04%), Space: 43 MB (28.74%) - LeetHub

--- 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.java
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.java
@@ -1,12 +1,17 @@
 class Solution {
     public int findPermutationDifference(String s, String t) {
+       Map<Character, Integer> map = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++){
+            map.put(s.charAt(i), i);
+        }
+
         int ans = 0;
-        for (int i = 0; i < s.length(); i++)
-            for (int j = 0; j < t.length(); j++)
-                if (s.charAt(i) == t.charAt(j)) {
-                    ans += Math.abs(i - j);
-                    break;
-                }
+
+        for(int i=0; i<t.length(); i++){
+            ans += Math.abs(i - map.get(t.charAt(i)));
+        }
+
         return ans;
     }
 }
