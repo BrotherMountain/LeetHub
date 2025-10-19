ba29e55606d95788ffe27ffef7bfc50ee8311ae6
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-10-19 14:42:21 +0900

    Time: 400 ms (62.48%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,14 @@
+# 단일 숫자 = 단 한 번만 숫자가 나옴 => COUNT(num) = 1
+# 가장 큰 DESC
+# 단일 숫자 LIMIT 1
+SELECT
+    MAX(num) AS num
+FROM (
+    SELECT 
+        num
+    FROM 
+        MyNumbers
+    GROUP BY
+        num
+    HAVING COUNT(num) = 1
+) AS maxnumber;
