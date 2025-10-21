9947b9647e06ce2f254a5a34604785c0f63e2082
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-10-21 22:53:37 +0900

    Time: 7 ms (96.91%), Space: 63.1 MB (50.58%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
@@ -1,27 +1,20 @@
 class Solution {
    public long maximumImportance(int n, int[][] roads) {
-    int[] degree = new int[n];
-    for (int[] road : roads) {
-      degree[road[0]]++;
-      degree[road[1]]++;
-    }
-    List<Integer> cities = new ArrayList<>();
-    for (int i = 0; i < n; i++) {
-      cities.add(i);
-    }
-    
-    cities.sort(new Comparator<Integer>() {
-      @Override
-      public int compare(Integer o1, Integer o2) {
-        return Integer.compare(degree[o2], degree[o1]);
-      }
-    });
-
     long ans = 0;
-    for (int i = 0; i < n; i++) {
-      ans += (long) (n - i) * degree[cities.get(i)];
+    long cnt =1;
+    long[] indegree = new long[n];
+
+    for(int[] e:roads){
+        indegree[e[0]]++;
+        indegree[e[1]]++;
     }
-    
+
+    Arrays.sort(indegree);
+
+    for(long i:indegree){
+        ans+=i*cnt;
+        cnt++;
+    }
     return ans;
   }
 }
