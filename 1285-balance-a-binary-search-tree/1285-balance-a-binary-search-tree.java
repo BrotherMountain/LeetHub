f996b2b09b1767279bc26b5f06199ce8861b1e84
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-10-27 22:33:49 +0900

    Time: 2 ms (97.75%), Space: 46 MB (61.47%) - LeetHub

--- 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.java
+++ 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.java
@@ -1,27 +1,29 @@
 class Solution {
-    public TreeNode balanceBST(TreeNode root) {
-        List<Integer> arr = new ArrayList<>();
-        inOrder(root, arr);
+  public TreeNode balanceBST(TreeNode root) {
+    List<Integer> list = new ArrayList<>();
+    inOrder(root, list);
+  
+    return buildTree(0, list.size() - 1, list);
+  }
 
-        return constructTree(0, arr.size() - 1, arr);
+  private TreeNode buildTree(int left, int right, List<Integer> list) {
+    if( left > right ) return null;
+
+    int mid = left + (right - left) / 2;
+    TreeNode root = new TreeNode(list.get(mid));
+    root.left = buildTree(left, mid - 1, list);
+    root.right = buildTree(mid + 1, right, list);
+
+    return root;
+  }
+
+  private void inOrder(TreeNode root, List<Integer> list) {
+    if(root == null){ 
+      return;
     }
-
-    private TreeNode constructTree(int l, int r, List<Integer> arr) {
-        if( l > r ) return null;
-
-        int mid = l + (r - l) / 2;
-        TreeNode root = new TreeNode(arr.get(mid));
-        root.left = constructTree(l, mid - 1, arr);
-        root.right = constructTree(mid + 1, r, arr);
-
-        return root;
-    }
-
-    private void inOrder(TreeNode root, List<Integer> arr) {
-        if(root == null) return;
-
-        inOrder(root.left, arr);
-        arr.add(root.val);
-        inOrder(root.right, arr);
-    }
+    
+    inOrder(root.left, list);
+    list.add(root.val);
+    inOrder(root.right, list);
+  }
 }
