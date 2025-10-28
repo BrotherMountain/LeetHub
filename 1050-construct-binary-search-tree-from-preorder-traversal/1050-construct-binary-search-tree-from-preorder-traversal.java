8fc96255e3664f3e29b99d507e2f409983782161
JongwonLee <74219749+BrotherMountain@users.noreply.github.com>
2025-10-28 22:59:40 +0900

    Time: 0 ms (100%), Space: 42.1 MB (20.68%) - LeetHub

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
@@ -0,0 +1,25 @@
+class Solution {
+    private int idx = 0;
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return build(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+private TreeNode build(int[] preorder, int lower, int upper) {
+        if (idx == preorder.length) {
+            return null;
+        }
+
+        int val = preorder[idx];
+        if (val < lower || val > upper) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(val);
+        
+        idx++;
+        root.left = build(preorder, lower, val);
+        root.right = build(preorder, val, upper);
+
+        return root;
+    }
+}
